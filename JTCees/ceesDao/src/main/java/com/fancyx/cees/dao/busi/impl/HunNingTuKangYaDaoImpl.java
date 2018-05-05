e188aafafebcbad7ff76709551aab84967b47638
woaixiaotimo <1084716575@qq.com>
2018-05-05 11:23:02 +0800

    提交之前未移交的混凝土抗压查询接口

---
+++ JTCees/ceesDao/src/main/java/com/fancyx/cees/dao/busi/impl/HunNingTuKangYaDaoImpl.java
@@ -0,0 +1,36 @@
+package com.fancyx.cees.dao.busi.impl;
+
+import com.fancyx.cees.dao.BaseDao;
+import com.fancyx.cees.dao.PageBean;
+import com.fancyx.cees.dao.busi.HunNingTuKangYaDao;
+import com.fancyx.cees.domain.busi.HunNingTuKangYa;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by 啊Q on 2018-05-02.
+ */
+@Repository
+public class HunNingTuKangYaDaoImpl extends BaseDao<HunNingTuKangYa> implements HunNingTuKangYaDao {
+    public static final String NAME_SPACE = "com.fancyx.cees.dao.busi.HunNingTuKangYaDao";
+
+    @Override
+    public PageBean<HunNingTuKangYa> pageQuery(PageBean pageBean, HunNingTuKangYa hunNingTuKangYa) {
+        PageBean<HunNingTuKangYa> hntkys = this.pageQuery(
+                this.getNamespace("selectByBean"),
+                this.getNamespace("selectByBeanCount"),
+                pageBean,
+                hunNingTuKangYa);
+        return hntkys;
+    }
+
+
+    /**
+     * mybatis的namespace
+     *
+     * @param statement
+     * @return 命名空间
+     */
+    private String getNamespace(String statement) {
+        return NAME_SPACE + "." + statement;
+    }
+}
